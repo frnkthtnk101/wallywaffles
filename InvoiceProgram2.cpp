da16eaa1f638da22561524c88056c227065b8171
c0demonkers@gmail.com <c0demonkers@gmail.com>
2019-01-20 01:24:56 +0000

    discount program

--- InvoiceProgram2.cpp
+++ InvoiceProgram2.cpp
@@ -3,16 +3,23 @@
 using namespace std;
 int main(){
 	int Quantity;
-	const float tax = .008;
+	const float TAX = .08;
+	const int DISCOUNT = 100;
+	const float DISCOUNTRATE = .2;
 	float price,total;
 	const unsigned int size = 10;
 	cout << "what how much of this product you buy and how much?"<<endl;
 	cin >> Quantity >> price;
-	total = (Quantity * price) + ((Quantity * price)*tax);
+	if(Quantity < DISCOUNT)
+		total = (Quantity * price) + ((Quantity * price)*TAX);
+	else
+		total = ((Quantity * price) - (Quantity * DISCOUNTRATE)) + ((Quantity * price)*TAX);
 	cout << "INVOICE\n";
 	cout << "Quality:"<<setw(size)<<Quantity<<endl;
 	cout << "Price  :"<<setw(size)<<price<<endl;
-	cout << "Tax    :"<<setw(size)<<tax<<endl;
+	cout << "Tax    :"<<setw(size)<<TAX<<endl;
 	cout << "Total  :"<<setw(size)<<total <<endl;
+	if(Quantity >= DISCOUNT)
+		cout << "Total Savings:"<<setw(size)<< ((Quantity * DISCOUNTRATE)) <<endl;
 	return 0;
 }
